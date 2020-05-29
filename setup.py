bb1dfdf10c0601eac558f393598a5812e7bb8cb6
Philip Bauer <bauer@starzel.de>
2020-05-29 09:28:27 +0200

    Back to development: 1.0a2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.relationhelpers',
-    version='1.0a1',
+    version='1.0a2.dev0',
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
