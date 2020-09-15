8aa460f70a9d056f2afb62bea284718205f7041f
Philip Bauer <bauer@starzel.de>
2020-09-15 16:25:26 +0200

    Back to development: 1.0a3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.relationhelpers',
-    version='1.0a2',
+    version='1.0a3.dev0',
     description="Helpers to manage, create, export and rebuild relations in Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
